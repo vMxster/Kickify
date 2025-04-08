760927c57ad6aa51977647f0d164e387e23b9ce1
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2025-04-08 10:20:53 +0200

    Updated LoginScreen.kt and OnboardScreen.kt

--- app/src/main/java/it/unibo/kickify/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/it/unibo/kickify/ui/screens/login/LoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,11 +22,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import it.unibo.kickify.R
+import it.unibo.kickify.ui.theme.KickifyFontFamily
+
+val kickifyFont = KickifyFontFamily
 
 @Preview
 @Composable
@@ -41,27 +46,39 @@ fun LoginScreen() {
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
-                text = stringResource(R.string.signinTitle),
+                text = stringResource(R.string.signin_Title),
                 color = Color.White,
                 fontSize = 28.sp,
+                fontFamily = kickifyFont,
                 modifier = Modifier.padding(bottom = 20.dp)
             )
             Text(
-                text = stringResource(R.string.signinText),
+                text = stringResource(R.string.signin_Text),
                 color = Color.White,
                 fontSize = 22.sp,
+                textAlign = TextAlign.Center,
+                fontFamily = kickifyFont,
                 modifier = Modifier.padding(bottom = 20.dp)
                     .padding(horizontal = 32.dp)
             )
             Text(
                 text = stringResource(R.string.signin_emailAddress),
                 color = Color.White,
+                fontFamily = kickifyFont,
                 modifier = Modifier.fillMaxWidth().padding(start = 32.dp)
             )
             OutlinedTextField(
                 value = "",
                 onValueChange = {},
-                label = { Text(stringResource(R.string.signin_emailAddress), color = Color.Gray) },
+                textStyle = TextStyle(
+                    fontFamily = kickifyFont
+                ),
+                label = {
+                    Text(
+                        stringResource(R.string.signin_emailAddress),
+                        color = Color.Gray,
+                        fontFamily = kickifyFont,
+                    ) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .clip(RoundedCornerShape(30.dp))
@@ -72,12 +89,21 @@ fun LoginScreen() {
             Text(
                 stringResource(R.string.signin_password),
                 color = Color.White,
+                fontFamily = kickifyFont,
                 modifier = Modifier.fillMaxWidth().padding(start = 32.dp)
             )
             OutlinedTextField(
                 value = "",
                 onValueChange = {},
-                label = { Text(stringResource(R.string.signin_emailAddress), color = Color.Gray) },
+                textStyle = TextStyle(
+                    fontFamily = kickifyFont
+                ),
+                label = {
+                    Text(
+                        stringResource(R.string.signin_emailAddress),
+                        color = Color.Gray,
+                        fontFamily = kickifyFont
+                    ) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .clip(RoundedCornerShape(30.dp))
@@ -86,6 +112,7 @@ fun LoginScreen() {
             Text(
                 text = stringResource(R.string.signin_forgotPassword),
                 color = Color.White,
+                fontFamily = kickifyFont,
                 modifier = Modifier.fillMaxWidth().padding(top = 10.dp, end = 32.dp),
                 textAlign = TextAlign.End
             )
@@ -97,7 +124,11 @@ fun LoginScreen() {
                     .fillMaxWidth()
                     .padding(horizontal = 32.dp),
             ) {
-                Text(text = stringResource(R.string.signin_button), color = Color.White)
+                Text(
+                    text = stringResource(R.string.signin_button),
+                    color = Color.White,
+                    fontFamily = kickifyFont,
+                )
             }
             Spacer(modifier = Modifier.height(15.dp))
             Button(
@@ -110,13 +141,26 @@ fun LoginScreen() {
                     painter = painterResource(R.drawable.google_icon),
                     contentDescription = "Login with Google")
                 Spacer(modifier = Modifier.width(10.dp))
-                Text(text = stringResource(R.string.signin_continueGoogle), color = Color.White)
+                Text(
+                    text = stringResource(R.string.signin_continueGoogle),
+                    color = Color.White,
+                    fontFamily = kickifyFont,
+                )
             }
-            Text(
-                text = stringResource(R.string.signin_joinForFree),
-                color = Color.White,
-                modifier = Modifier.padding(bottom = 20.dp, top = 20.dp)
-            )
+
+            TextButton(
+                onClick = { /* go to register page */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.signin_joinForFree),
+                    color = Color.White,
+                    fontFamily = kickifyFont,
+                    modifier = Modifier.padding(bottom = 20.dp, top = 20.dp)
+                )
+            }
         }
     }
 }
--- app/src/main/java/it/unibo/kickify/ui/screens/onboard/OnboardScreen.kt
+++ app/src/main/java/it/unibo/kickify/ui/screens/onboard/OnboardScreen.kt
@@ -1,13 +1,10 @@
 package it.unibo.kickify.ui.screens.onboard
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,27 +12,27 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import it.unibo.kickify.R
 import it.unibo.kickify.ui.composables.ExpandingDotIndicator
+import it.unibo.kickify.ui.theme.KickifyFontFamily
 
-val kickifyFontFamily = FontFamily(
-    Font(R.font.alexandria_bold, FontWeight.Bold),
-    Font(R.font.alexandria_regular, FontWeight.Normal)
-)
+val kickifyFont = KickifyFontFamily
 
 @Preview
 @Composable
@@ -44,9 +41,10 @@ fun PagerWithIndicator() {
     var selectedIndex by remember { mutableIntStateOf(0) }
 
     Column (
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxSize()
+            .padding(horizontal = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
+        verticalArrangement = Arrangement.Bottom
     ){
         Image(
             painter = when(selectedIndex){
@@ -54,39 +52,41 @@ fun PagerWithIndicator() {
                 2 -> painterResource(R.drawable.newbalance_onboard)
                 else -> painterResource(R.drawable.adidas_onboard)
             },
-            contentDescription = "Welcome page")
-        Row{
-            Text(when (selectedIndex){
-                0 -> stringResource(R.string.onboard_title_1)
-                1 -> stringResource(R.string.onboard_title_2)
-                2 -> stringResource(R.string.onboard_title_3)
-                else -> " "
-            }, style = TextStyle(
-                fontSize = 24.sp,
-                fontFamily = kickifyFontFamily,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-        Row{
-            Text( when (selectedIndex) { // messages
-                0 -> stringResource(R.string.onboard_text_1)
-                1 -> stringResource(R.string.onboard_text_2)
-                2 -> stringResource(R.string.onboard_text_3)
-                else -> " "
-            }, style = TextStyle(
-                fontSize = 10.sp,
-                fontFamily = kickifyFontFamily,
-                fontWeight = FontWeight.Normal,
-                color = Color.White
-            )
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-        Row (
             modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth,
+            contentDescription = "Welcome page"
+        )
+        Text(when (selectedIndex){
+            0 -> stringResource(R.string.onboard_title_1)
+            1 -> stringResource(R.string.onboard_title_2)
+            2 -> stringResource(R.string.onboard_title_3)
+            else -> " "
+        }, style = TextStyle(
+            fontSize = 30.sp,
+            fontFamily = kickifyFont,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Text( when (selectedIndex) { // messages
+            0 -> stringResource(R.string.onboard_text_1)
+            1 -> stringResource(R.string.onboard_text_2)
+            2 -> stringResource(R.string.onboard_text_3)
+            else -> " "
+        }, style = TextStyle(
+            fontSize = 22.sp,
+            fontFamily = kickifyFont,
+            fontWeight = FontWeight.Normal,
+            color = Color.White
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row (
+            modifier = Modifier.fillMaxWidth().padding(bottom = 20.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             ExpandingDotIndicator(
@@ -99,13 +99,15 @@ fun PagerWithIndicator() {
 
             Button(onClick = {
                 selectedIndex = (selectedIndex + 1) % totalDots
+                if(selectedIndex == (totalDots - 1)){ // when on last page
+                }
             }) {
                 Text(when(selectedIndex) {
                     0 -> stringResource(R.string.getStarted)
                     else -> stringResource(R.string.nextPage)
                 }, style = TextStyle(
-                    fontFamily = kickifyFontFamily,
-                )
+                    fontFamily = kickifyFont,
+                    )
                 )
             }
         }
