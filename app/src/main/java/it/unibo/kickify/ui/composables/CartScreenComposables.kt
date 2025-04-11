22062f9aa25f40de2d59478ee0755509417ede3e
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2025-04-11 20:12:51 +0200

    Update CartScreen.kt.

--- app/src/main/java/it/unibo/kickify/ui/composables/CartScreenComposables.kt
+++ app/src/main/java/it/unibo/kickify/ui/composables/CartScreenComposables.kt
@@ -1,9 +1,9 @@
 package it.unibo.kickify.ui.composables
 
-import android.app.Dialog
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
@@ -36,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -44,7 +46,7 @@ import androidx.compose.ui.unit.dp
 import it.unibo.kickify.R
 
 @Composable
-fun CartItem(itemName: String, price: Double, size: Int
+fun CartItem(itemName: String, price: Double, size: Int, productColor: Color
 ){
     Card(
         //onClick = onClick,
@@ -92,6 +94,8 @@ fun CartItem(itemName: String, price: Double, size: Int
                         "€$price",
                         style = MaterialTheme.typography.bodyMedium
                     )
+                    Spacer(Modifier.width(10.dp))
+                    ShoesColorIndicator(productColor)
                 }
                 Spacer(Modifier.height(5.dp))
                 QuantityManager()
@@ -220,4 +224,13 @@ fun CartResume(subTotal: Double, shipping: Double){
             }
         }
     }
+}
+
+@Composable
+fun ShoesColorIndicator(productColor: Color){
+    Box(Modifier
+        .size(20.dp)
+        .clip(CircleShape)
+        .background(productColor)
+    )
 }
