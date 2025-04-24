04341d3751739391c16d244a6fe9d1fd7e5a2840
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2025-04-24 15:35:24 +0200

    Update dependencies, Manifest and translations.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
+    implementation(libs.androidx.compose.material3.material3)
     implementation(libs.material3)
     implementation(libs.androidx.material3.adaptive.navigation.suite)
+
+    /* for biometric authentication */
+    implementation(libs.androidx.biometric.v120alpha05)
 }
