522078b452e460d210ddc627e4087a6dda0f6dea
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2025-06-18 11:19:11 +0000

    Fixed product list in CartScreen.

--- app/src/main/java/it/unibo/kickify/ui/screens/cart/CartScreen.kt
+++ app/src/main/java/it/unibo/kickify/ui/screens/cart/CartScreen.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -17,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
@@ -75,44 +79,53 @@ fun CartScreen(
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
+            verticalArrangement = Arrangement.Bottom
         ) {
-            if(cartItems.isEmpty()){
-                Text(stringResource(R.string.emptyCart))
-            } else {
-                cartItems.forEach { item ->
-                    var imgUrl = ""
-                    productImages.onSuccess { l ->
-                        val img = l.entries.firstOrNull { entry -> entry.key.productId == item.cartProduct.productId }
-                        imgUrl = img?.value?.url ?: ""
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth().weight(1f)
+            ) {
+                if (cartItems.isEmpty()) {
+                    item {
+                        Text(stringResource(R.string.emptyCart),
+                            modifier =  Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center)
                     }
-                    CartItem(
-                        itemName = item.nome,
-                        price = item.prezzo,
-                        size = item.cartProduct.size.toInt(),
-                        productColor = item.cartProduct.color,
-                        imageUrl = imgUrl,
-                        quantity = item.cartProduct.quantity,
-                        onChangeQuantity = { newQty ->
-                            cartViewModel.updateQuantity(
-                                productId = item.cartProduct.productId,
-                                color = item.cartProduct.color,
-                                size = item.cartProduct.size,
-                                newQuantity = newQty
-                            )
-                        },
-                        onDelete = {
-                            cartViewModel.removeFromCart(
-                                productId = item.cartProduct.productId,
-                                color = item.cartProduct.color,
-                                size = item.cartProduct.size
-                            )
+                } else {
+                    items(cartItems.sortedBy { it.nome }) { item ->
+                        var imgUrl = ""
+                        productImages.onSuccess { l ->
+                            val img =
+                                l.entries.firstOrNull { entry -> entry.key.productId == item.cartProduct.productId }
+                            imgUrl = img?.value?.url ?: ""
                         }
-                    )
-                    Spacer(Modifier.height(15.dp))
+                        Spacer(Modifier.height(15.dp))
+                        CartItem(
+                            itemName = item.nome,
+                            price = item.prezzo,
+                            size = item.cartProduct.size.toInt(),
+                            productColor = item.cartProduct.color,
+                            imageUrl = imgUrl,
+                            quantity = item.cartProduct.quantity,
+                            onChangeQuantity = { newQty ->
+                                cartViewModel.updateQuantity(
+                                    productId = item.cartProduct.productId,
+                                    color = item.cartProduct.color,
+                                    size = item.cartProduct.size,
+                                    newQuantity = newQty
+                                )
+                            },
+                            onDelete = {
+                                cartViewModel.removeFromCart(
+                                    productId = item.cartProduct.productId,
+                                    color = item.cartProduct.color,
+                                    size = item.cartProduct.size
+                                )
+                            }
+                        )
+                    }
                 }
             }
-            Spacer(Modifier.height(20.dp))
+            Spacer(Modifier.height(12.dp))
 
             CartAndCheckoutResume(
                 subTotal = subTotal,
